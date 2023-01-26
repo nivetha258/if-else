09e389a58d2a64c971a14d046900ba2a08cc8db3
nivetha <nivetharajendran1994@gmail.com>
2023-01-26 10:47:57 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+
+let age = 17;
+let test = "pass";
+let adhaar = "yes";
+
+if(age >=18 && test == "pass")
+{
+	console.log("you are eligible for license")
+}
+else {
+	console.log("you are not eligible for license")
+}
+
+if(age >=18 && adhaar == "yes")
+{
+	console.log("you are eligible for voting")
+}
+else {
+	console.log("you are not eligible for voting")
+}
+	
+	
+	
+	
+	
+	
+	
